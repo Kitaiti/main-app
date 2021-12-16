e417d82c57dbcb0732206c05bd7a0427926d0c77
Kitaiti <92067023+Kitaiti@users.noreply.github.com>
2021-12-16 16:33:51 +0900

    main

--- src/App.js
+++ src/App.js
@@ -59,16 +59,6 @@ const App = () => {
     calculateTotal();
 
     // OGP
-    const url = { inputValue };
-    fetch(url).then(res => res.text()).then(text => {
-      const el = new DOMParser().parseFromString(text, "text/html")
-      const headEls = (el.head.children)
-      Array.from(headEls).map(v => {
-        const prop = v.getAttribute('property')
-        if (!prop) return;
-        console.log(prop, v.getAttribute("content"))
-      })
-    })
   }
 
   // done切り替え
